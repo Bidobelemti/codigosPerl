f8e137001969ffd957357b20b98802e0369b806f
Bidobelemti <mauriceuve3221@gmail.com>
2025-05-09 19:18:13 -0500

    Inicio de uso de libreria MXNet

---
+++ Tarea2_uso.pl
@@ -0,0 +1,8 @@
+use string;
+use warning;
+use Data::Dump qw(dump);
+use AI::MXNet qw(mx)
+use Chart::Plotly;
+use Chart::Plotly::Plot;
+use Chart::Plotly::Trace::Scatter;
+IPerl->load_plugin('Chart::Plotly');
